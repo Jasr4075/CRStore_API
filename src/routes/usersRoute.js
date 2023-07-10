99faad3b65759d4c03578c10eeecf4b8d8f0e1eb
parxivall <jasr4075@gmail.com>
2023-07-10 18:22:18 -0300

    hoy

--- src/routes/usersRoute.js
+++ src/routes/usersRoute.js
@@ -1,9 +1,9 @@
-import controller from '../controllers/usersController'
+import controller from "../controllers/usersController";
 
 export default (app) => {
-	app.post('/users/validate', controller.adminLogged)
-	app.get('/users', controller.getAll)
-	app.post('/users/register', controller.register)
-	app.post('/users/login', controller.login)
-	app.post('/users/:id', controller.deleteUser)
-}
+  app.post("/users/validate", controller.adminLogged);
+  app.get("/users", controller.getAll);
+  app.post("/users/register", controller.register);
+  app.post("/users/login", controller.login);
+  app.delete("/users/:id", controller.deleteUser); // Cambiado de app.post a app.delete
+};
